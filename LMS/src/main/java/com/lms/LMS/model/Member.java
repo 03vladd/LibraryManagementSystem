9f2c2e587070e7a0e6c2127cb333987a1a8aa8b6
raresica <raresrahaian1@gmail.com>
2025-10-27 23:33:31 +0200

    Completed Member class

--- LMS/src/main/java/com/lms/LMS/model/Member.java
+++ LMS/src/main/java/com/lms/LMS/model/Member.java
@@ -1,16 +1,33 @@
 package com.lms.LMS.model;
 
+import java.util.ArrayList;
 import java.util.List;
-import com.lms.LMS.model.Reservation;
-import com.lms.LMS.model.Loan;
 
 public class Member {
     private String id;
     private String name;
-    private String LibraryId;
-    private List<Reservation> reservations;
+    private String libraryId;
+    private String address;
     private List<Loan> loans;
 
+    // NEW PROPERTIES ADDED
+    private String phoneNumber;
+    private String email;
+
+    // Constructor
+    public Member() {
+        this.loans = new ArrayList<>();
+    }
+
+    public Member(String id, String name, String libraryId, String address) {
+        this.id = id;
+        this.name = name;
+        this.libraryId = libraryId;
+        this.address = address;
+        this.loans = new ArrayList<>();
+    }
+
+    // Getters and Setters
     public String getId() {
         return id;
     }
@@ -28,10 +45,47 @@ public class Member {
     }
 
     public String getLibraryId() {
-        return LibraryId;
+        return libraryId;
     }
 
     public void setLibraryId(String libraryId) {
-        LibraryId = libraryId;
+        this.libraryId = libraryId;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Loan> getLoans() {
+        return loans;
+    }
+
+    public void setLoans(List<Loan> loans) {
+        this.loans = loans;
+    }
+
+    public void addLoan(Loan loan) {
+        this.loans.add(loan);
+    }
+
+    // NEW GETTERS AND SETTERS
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+}
 }
