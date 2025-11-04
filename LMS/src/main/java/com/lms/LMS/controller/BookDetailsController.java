54df5ea03fe903143490ff113de80e5aa58227fa
Vlad <vlad.vasiu@stud.ubbcluj.ro>
2025-11-04 10:46:32 +0300

    Created Controllers for all Entities. Refactored ReadableItems using Lombok as well.

--- LMS/src/main/java/com/lms/LMS/controller/BookDetailsController.java
+++ LMS/src/main/java/com/lms/LMS/controller/BookDetailsController.java
@@ -1,13 +1,42 @@
 package com.lms.LMS.controller;
 
+import com.lms.LMS.model.BookDetails;
+import com.lms.LMS.service.BookDetailsService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/books")
 public class BookDetailsController {
-    @GetMapping("/BookDetails")
-    public String testBookDetails() {
-        return "Hello Book Details - Book Details endpoint is running!";
+
+    private final BookDetailsService bookDetailsService;
+
+    public BookDetailsController(BookDetailsService bookDetailsService) {
+        this.bookDetailsService = bookDetailsService;
     }
 
+    @GetMapping
+    public String listBooks(Model model) {
+        model.addAttribute("books", bookDetailsService.getAllBooks());
+        return "bookdetails/index";
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("book", new BookDetails());
+        return "bookdetails/form";
+    }
+
+    @PostMapping
+    public String createBook(@ModelAttribute BookDetails book) {
+        bookDetailsService.saveBook(book);
+        return "redirect:/books";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteBook(@PathVariable String id) {
+        bookDetailsService.deleteBook(id);
+        return "redirect:/books";
+    }
 }
--- LMS/src/main/java/com/lms/LMS/controller/ReadableItemsController.java
+++ LMS/src/main/java/com/lms/LMS/controller/ReadableItemsController.java
@@ -1,14 +1,42 @@
 package com.lms.LMS.controller;
 
+import com.lms.LMS.model.ReadableItems;
+import com.lms.LMS.service.ReadableItemService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-
+@RequestMapping("/items")
 public class ReadableItemsController {
-    @GetMapping("/ReadableItems")
-    public String testReadableItems() {
-        return "Hello Readable Items - Readable Items endpoint is running!";
+
+    private final ReadableItemService readableItemService;
+
+    public ReadableItemsController(ReadableItemService readableItemService) {
+        this.readableItemService = readableItemService;
+    }
+
+    @GetMapping
+    public String listItems(Model model) {
+        model.addAttribute("items", readableItemService.getAllReadableItems());
+        return "readableitems/index";
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("item", new ReadableItems());
+        return "readableitems/form";
+    }
+
+    @PostMapping
+    public String createItem(@ModelAttribute ReadableItems item) {
+        readableItemService.saveReadableItem(item);
+        return "redirect:/items";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteItem(@PathVariable String id) {
+        readableItemService.deleteReadableItem(id);
+        return "redirect:/items";
     }
 }
-
