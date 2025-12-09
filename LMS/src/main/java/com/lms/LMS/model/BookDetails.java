9406cd5aba12c27674953bc2710feca4d62e2359
Vlad <vlad.vasiu@stud.ubbcluj.ro>
2025-12-09 10:00:05 +0300

    Made it work.

--- LMS/src/main/java/com/lms/LMS/model/BookDetails.java
+++ LMS/src/main/java/com/lms/LMS/model/BookDetails.java
@@ -2,32 +2,21 @@ package com.lms.LMS.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.ToString;
-import java.util.*;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@DiscriminatorValue("BOOK")
 @Data
-@ToString(exclude = "bookAuthors")
+@NoArgsConstructor
 public class BookDetails extends Publication {
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BookAuthor> bookAuthors = new ArrayList<>();
 
-    @OneToMany(mappedBy = "bookDetails", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<BookAuthor> bookAuthors;
-
-    public BookDetails() {
-        super();
+    public BookDetails(String title, List<ReadableItems> copies) {
+        super(null, title, copies);
         this.bookAuthors = new ArrayList<>();
-    }
-
-    public BookDetails(String id, String title, List<ReadableItems> copies) {
-        super(id, title, copies);
-        this.bookAuthors = new ArrayList<>();
-    }
-
-    public List<BookAuthor> getBookAuthors() {
-        return bookAuthors;
-    }
-
-    public void setBookAuthors(List<BookAuthor> bookAuthors) {
-        this.bookAuthors = bookAuthors;
     }
 }
